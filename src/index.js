a05acfc77caa27f9d38bada7a8cd0da7558658c0
LeeSeul <mary567@hanmail.net>
2019-11-11 12:47:25 +0900

    페이지설정

--- src/index.js
+++ src/index.js
@@ -4,8 +4,4 @@ import Root from './Root';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
---
+++ src/pages/ListPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from 'components/Header';
+
+const ListPage = () => {
+    return (
+        <div>
+            <Header />
+            List
+        </div>
+    );
+};
+
+export default ListPage;
